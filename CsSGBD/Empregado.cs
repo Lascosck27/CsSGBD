a1126822f4f6f8e5d0fb8c9647ebf4514f07ea0a
Lascosck27 <gabriellascosck0909@gmail.com>
2024-02-27 21:03:25 -0300

    Programa pronto

---
+++ CsSGBD/Empregado.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace CsSGBD
+{
+    class Empregado
+    {
+        private const string stringConexao = "SERVER=localhost; PORT=3306; DATABASE=cssgbd_db; UID=root; PWD=L4sc0sck!";
+
+        public int Matricula {  get; set; }
+        public string Cpf { get; set;}
+        public string Nome { get; set;}
+        public string Endereco { get; set;}
+
+        public DataTable Pesquisar()
+        {
+            try
+            {
+                StringBuilder stringSql = new StringBuilder();  //cria uma instância da classe StringBuilder, chamada stringSql
+                stringSql.Append("select matricula, cpf, nome, endereco "); //seleciona os as colunas matricula, cpf, nome e endereco
+                stringSql.Append("from empregado "); //da tabela empregado
+                stringSql.Append("where 1");    //sempre verdadeiro
+
+                if (Matricula != 0)     //se Matricula for diferente de zero: adiciona a clausula AND, selecionando somente registros com a matricula especificada
+                    stringSql.Append(" and matricula=@matricula");
+                if (!Cpf.Equals(""))    //se Cpf não estiver vazio: seleciona apenas registros com o CPF especificado
+                    stringSql.Append(" and cpf=@cpf");
+                if (!Nome.Equals("")) 
+                    stringSql.Append(" and nome like @nome"); /* o operador LIKE permite correspondências parciais nos nomes dos registros,
+                                                               * o que permite que registros com nomes que contenham a sequência de caracteres 
+                                                               * especificada serão selecionados. Ex: se o nome for 'João', a parte resultante será:
+                                                               * "AND nome like 'João'", onde % é um caractere curinga que corresponde à qualquer sequência
+                                                               * de caracteres antes ou depois do nome especificado.*/
+
+                MySqlConnection conexao = new MySqlConnection(stringConexao); /* a classe MySqlConnection cria uma conexão com o DB. O construtor recebe como parâmetro uma sring de 
+                                                                               * conexão, que neste caso é a stringConexao, que contém as informações necessárias para fazer a conexão*/
+
+                MySqlCommand comando = new MySqlCommand(stringSql.ToString(), conexao); /* a classe MySqlCommand cria um novo comando SQL. O construtor recebeu dois parâmtros:
+                                                                                         * a consulta SQL em forma de string (através do ToString do StringBuilder stringSql)
+                                                                                         * e a conexão com o banco de dados (conexao)*/
+
+                comando.Parameters.AddWithValue("@matricula", Matricula);   //adiciona um parâmetro chamado @matricula ao comando, com o valor de Matricula
+                comando.Parameters.AddWithValue("@cpf", Cpf);   //adiciona um parâmetro chamado @cpf ao comando, com o valor de Cpf
+                comando.Parameters.AddWithValue("@nome", "%"+Nome+"%"); //adiciona um parâmetro chamado @nome ao comando, com o valor de Nome, mas modificado com o %%.
+
+                MySqlDataAdapter adaptador = new MySqlDataAdapter(); //cria um adaptador de dados usando a classe MySqlDataAdapter
+                adaptador.SelectCommand = comando; //define como comando o 'comando' (criado anteriormente)
+
+                DataTable dados = new DataTable(); //cria um objeto DataTable para armazenar os resultados da consulta
+                adaptador.Fill(dados); //executa a consulta usando o adaptador e preenche o DataTable com os resultados
+                return dados; //como resultado da função, o DT contendo os dados é retornado
+
+            }
+
+            catch (Exception e) //captura uma exceção, se houver, e lança ao usuário.
+            {
+                throw e;
+            }
+        }
+
+        public void Incluir()
+        {
+            try
+            {
+                MySqlConnection conexao = new MySqlConnection(stringConexao);   /* a classe MySqlConnection cria uma conexão com o DB, utilizando stringConexao como
+                                                                                 * a string de conexão especificada*/
+                MySqlCommand comando = new MySqlCommand();  //cria um novo comando SQL
+                comando.Connection = conexao;   //define a conexão que será usada pelo comando
+                comando.CommandText = "insert into empregado (cpf, nome, endereco) " + 
+                                                    "values (@cpf, @nome, @endereco)"; /* Define a instrução SQL de inserção, especificando as colunas e
+                                                                                        * os parâmetros que serão inseridos na tabela "empregado"*/
+                comando.Parameters.AddWithValue("@cpf", Cpf); // atribui o valor da variável Cpf ao parâmetro @cpf no comando SQL
+                comando.Parameters.AddWithValue("@nome", Nome); // atribui o valor da variável Nome ao parâmetro @nome 
+                comando.Parameters.AddWithValue("@endereco", Endereco); // atribui o valor da variável Endereco ao parâmetro @endereco
+
+                conexao.Open(); // abre a conexão com o banco de dados
+                comando.Prepare();  // prepara o comando SQL para execução. Importante para otimizar o desempenho e evitar ataques de injeção de SQL
+                comando.ExecuteNonQuery(); // executa o comando para inserir os dados na tabela. Como se trata de um comando de inserção, não são esperados resultados retornados
+                conexao.Close(); // fecha a conexão com o DB
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void Alterar()
+        {
+            try
+            {
+                MySqlConnection conexao = new MySqlConnection(stringConexao);
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = conexao;
+                comando.CommandText = "update empregado SET " +
+                                              "cpf=@cpf, " +
+                                              "nome=@nome, " +
+                                              "endereco=@endereco, " +
+                                              "where matricula=@matricula";
+
+                comando.Parameters.AddWithValue("@matricula", Matricula);
+                comando.Parameters.AddWithValue("@cpf", Cpf);
+                comando.Parameters.AddWithValue("@nome", Nome);
+                comando.Parameters.AddWithValue("@endereco", Endereco);
+
+                conexao.Open();
+                comando.Prepare();
+                comando.ExecuteNonQuery();
+                conexao.Close();
+
+            }
+            catch (Exception e) 
+            { 
+                throw e;
+            }
+        }
+
+        public void Excluir()
+        {
+            try
+            {
+                MySqlConnection conexao = new MySqlConnection(stringConexao);
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = conexao;
+                comando.CommandText = "delete from empregado " +
+                                       "where matricula=@matricula";
+
+                comando.Parameters.AddWithValue("@matricula", Matricula);
+
+                conexao.Open();
+                comando.Prepare();
+                comando.ExecuteNonQuery();
+                conexao.Close();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ CsSGBD/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace CsSGBD
+{
+    partial class FormLocal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLocal));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonPesquisar = new System.Windows.Forms.Button();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.buttonExcluir = new System.Windows.Forms.Button();
+            this.buttonLimpar = new System.Windows.Forms.Button();
+            this.dataGridViewEmpregado = new System.Windows.Forms.DataGridView();
+            this.textMatricula = new System.Windows.Forms.TextBox();
+            this.textCpf = new System.Windows.Forms.TextBox();
+            this.textNome = new System.Windows.Forms.TextBox();
+            this.textEndereco = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpregado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastro de empregados";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Matrícula";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CPF";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nome";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Endereço";
+            // 
+            // buttonPesquisar
+            // 
+            this.buttonPesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPesquisar.Location = new System.Drawing.Point(590, 83);
+            this.buttonPesquisar.Name = "buttonPesquisar";
+            this.buttonPesquisar.Size = new System.Drawing.Size(95, 30);
+            this.buttonPesquisar.TabIndex = 5;
+            this.buttonPesquisar.Text = "Pesquisar";
+            this.buttonPesquisar.UseVisualStyleBackColor = true;
+            this.buttonPesquisar.Click += new System.EventHandler(this.buttonPesquisar_Click);
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSalvar.Location = new System.Drawing.Point(590, 119);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(95, 30);
+            this.buttonSalvar.TabIndex = 6;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // buttonExcluir
+            // 
+            this.buttonExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExcluir.Location = new System.Drawing.Point(590, 155);
+            this.buttonExcluir.Name = "buttonExcluir";
+            this.buttonExcluir.Size = new System.Drawing.Size(95, 30);
+            this.buttonExcluir.TabIndex = 7;
+            this.buttonExcluir.Text = "Excluir";
+            this.buttonExcluir.UseVisualStyleBackColor = true;
+            this.buttonExcluir.Click += new System.EventHandler(this.buttonExcluir_Click);
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLimpar.Location = new System.Drawing.Point(590, 191);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(95, 30);
+            this.buttonLimpar.TabIndex = 8;
+            this.buttonLimpar.Text = "Limpar";
+            this.buttonLimpar.UseVisualStyleBackColor = true;
+            this.buttonLimpar.Click += new System.EventHandler(this.buttonLimpar_Click);
+            // 
+            // dataGridViewEmpregado
+            // 
+            this.dataGridViewEmpregado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmpregado.Location = new System.Drawing.Point(22, 245);
+            this.dataGridViewEmpregado.Name = "dataGridViewEmpregado";
+            this.dataGridViewEmpregado.Size = new System.Drawing.Size(663, 192);
+            this.dataGridViewEmpregado.TabIndex = 9;
+            this.dataGridViewEmpregado.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewEmpregado_CellContentClick);
+            // 
+            // textMatricula
+            // 
+            this.textMatricula.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textMatricula.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textMatricula.Location = new System.Drawing.Point(117, 87);
+            this.textMatricula.Name = "textMatricula";
+            this.textMatricula.ReadOnly = true;
+            this.textMatricula.Size = new System.Drawing.Size(100, 26);
+            this.textMatricula.TabIndex = 10;
+            // 
+            // textCpf
+            // 
+            this.textCpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textCpf.Location = new System.Drawing.Point(117, 119);
+            this.textCpf.Name = "textCpf";
+            this.textCpf.Size = new System.Drawing.Size(100, 26);
+            this.textCpf.TabIndex = 11;
+            // 
+            // textNome
+            // 
+            this.textNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNome.Location = new System.Drawing.Point(117, 149);
+            this.textNome.Name = "textNome";
+            this.textNome.Size = new System.Drawing.Size(100, 26);
+            this.textNome.TabIndex = 12;
+            // 
+            // textEndereco
+            // 
+            this.textEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEndereco.Location = new System.Drawing.Point(117, 179);
+            this.textEndereco.Name = "textEndereco";
+            this.textEndereco.Size = new System.Drawing.Size(100, 26);
+            this.textEndereco.TabIndex = 13;
+            // 
+            // FormLocal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(706, 459);
+            this.Controls.Add(this.textEndereco);
+            this.Controls.Add(this.textNome);
+            this.Controls.Add(this.textCpf);
+            this.Controls.Add(this.textMatricula);
+            this.Controls.Add(this.dataGridViewEmpregado);
+            this.Controls.Add(this.buttonLimpar);
+            this.Controls.Add(this.buttonExcluir);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(this.buttonPesquisar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormLocal";
+            this.Text = "Aplicação local C# com SGBD";
+            this.Load += new System.EventHandler(this.FormLocal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpregado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonPesquisar;
+        private System.Windows.Forms.Button buttonSalvar;
+        private System.Windows.Forms.Button buttonExcluir;
+        private System.Windows.Forms.Button buttonLimpar;
+        private System.Windows.Forms.DataGridView dataGridViewEmpregado;
+        private System.Windows.Forms.TextBox textMatricula;
+        private System.Windows.Forms.TextBox textCpf;
+        private System.Windows.Forms.TextBox textNome;
+        private System.Windows.Forms.TextBox textEndereco;
+    }
+}
+
